e1313467b364cf6f2d1f7e07d351a4e3d539e008
Antonino <121946938+LeAntonino@users.noreply.github.com>
2025-08-06 10:49:57 -0300

    {solução concluída do {exercisio 12: Juntando duas listas}}

---
+++ Juntando_duas_listas.py
@@ -0,0 +1,9 @@
+def junta(list1, list2):
+    list3 = list1 + list2
+    return list3
+
+
+lista1 = ["rio", "floresta", "leão", "montanha", "oceano", "estrela", "vento", "cachoeira", "lua", "girassol"]
+lista2 = ["algoritmo", "API", "blockchain", "código", "software", "nuvem", "hardware", "interface", "pixel", "servidor"]
+
+print(junta(lista1, lista2))
