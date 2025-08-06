b624925abb9385a04fecd338170cf4e4f93f0ede
Antonino <121946938+LeAntonino@users.noreply.github.com>
2025-08-06 10:46:31 -0300

    {solução concluída do {exercisio 11: Verificando se uma lista está vazia}}

---
+++ Verificiando_se_uma_lista_está_vazia.py
@@ -0,0 +1,12 @@
+def vazia(lista):
+    if len(lista) == 0: 
+        return True
+    else: 
+        return False
+    
+
+lista1 = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+lista2 = []
+
+print(vazia(lista1))
+print(vazia(lista2))
