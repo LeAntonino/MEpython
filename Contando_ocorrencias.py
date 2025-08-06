33690a6caa4264705f7a821bb9e9e533484c6971
Antonino <121946938+LeAntonino@users.noreply.github.com>
2025-08-06 10:36:42 -0300

    {solução concluída do {exercisio 9: Contando ocorrências}}

---
+++ Contando_ocorrencias.py
@@ -0,0 +1,10 @@
+def ocorrencias(list,value):
+    total = 0
+    for i in range(0,len(list)):
+        if list[i] == value:
+            total +=1
+    return total
+
+
+lista = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+print(ocorrencias(lista,9))
