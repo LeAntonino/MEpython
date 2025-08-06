2a42bac9e76f80040d74a3e3667734183fdb300c
Antonino <121946938+LeAntonino@users.noreply.github.com>
2025-08-06 09:48:36 -0300

    {solução concluída do {exercísio 5: Encontrando os números ímpares}}

---
+++ Encontrando_os_números_ímpares.py
@@ -0,0 +1,10 @@
+def pares(list):
+    listapares = []
+    for i in range(0,len(list)):
+        if (list[i]%2) != 0:
+            listapares.append(list[i])
+    return(listapares)
+
+
+lista = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+print(pares(lista))
